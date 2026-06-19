8e4885b8c922e2cd7f9c42e6830f1a944892a555
Gabrielcotts <gabrielcotts1444@gmail.com>
2026-06-19 13:50:46 -0300

    Concluindo a versão final do QUIZ

--- FaseUm_01-06-2026/Quiz.c
+++ FaseUm_01-06-2026/Quiz.c
@@ -1,136 +1,216 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-#define TOTAL 10
-
-typedef struct {
-	char pergunta[200];
-	char alternativas[4][100];
-	char correta;
-} Pergunta;
-
-
-void Quiz() {
-	Pergunta quiz[TOTAL] = {
-		{
-			"Qual foi a primeira capital do Brasil?",
-			{"Salvador", "Brasilia", "Rio de Janeiro", "Sao Paulo"},
-			'A'
-		},
-		{
-			"Qual a raiz cubica de 343?",
-			{"27", "343", "9", "7"},
-			'D'
-		},
-		{
-			"Qual o maior planeta do Sistema Solar?",
-			{"Terra", "Marte", "Jupiter", "Saturno"},
-			'C'
-		},
-		{
-			"Em que continente fica o Egito?",
-			{"Asia", "Africa", "Europa", "Oceania"},
-			'B'
-		},
-		{
-			"Qual o elemento quimico representado pelo simbolo 'O'?",
-			{"Ouro", "Osmio", "Oxigenio", "Hidrogenio"},
-			'C'
-		},
-		{
-			"Quem pintou a obra Mona Lisa?",
-			{"Vincent Van Gogh", "Pablo Picasso", "Michelangelo", "Leonardo da Vinci"},
-			'D'
-		},
-		{
-			"Qual eh o oceano que banha a costa leste do Brasil?",
-			{"Oceano Indico", "Oceano Pacifico", "Oceano Artico", "Oceano Atlantico"},
-			'D'
-		},
-		{
-			"Quantos lados tem um hexagono?",
-			{"5", "6", "7", "8"},
-			'B'
-		},
-		{
-			"Qual destes animais eh um mamifero?",
-			{"Tubarao", "Pinguim", "Golfinho", "Tartaruga"},
-			'C'
-		},
-		{
-			"Qual e o menor numero primo?",
-			{"0", "1", "2", "3"},
-			'C'
-		}
-	};
-
-	char resposta;
-	int acertos = 0;
-	int erros = 0;
-	int i;
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <ctype.h>
 	
+	#define TOTAL 10
+	
+	int tentativas = 0;
+	int repeticoesPergunta[TOTAL] = {0};
+	int repeticoesNivel[3] = {0};
+	
+	void Quiz();
+	
+	typedef struct{
+	    char pergunta[200];
+	    char alternativas[4][100];
+	    char correta;
+	}Pergunta;
+	
+	void Quiz(){
+	
+		tentativas++;
+	
+	    Pergunta quiz[TOTAL] = {
+	
+	        {
+	            "Qual foi a primeira capital do Brasil?",
+	            {"Salvador","Brasilia","Rio de Janeiro","Sao Paulo"},
+	            'A'
+	        },
+	
+	        {
+	            "Qual a raiz cubica de 343?",
+	            {"27","343","9","7"},
+	            'D'
+	        },
+	
+	        {
+	            "Qual o maior planeta do Sistema Solar?",
+	            {"Terra","Marte","Jupiter","Saturno"},
+	            'C'
+	        },
+	
+	        {
+	            "Em que continente fica o Egito?",
+	            {"Asia","Africa","Europa","Oceania"},
+	            'B'
+	        },
+	
+	        {
+	            "Qual o elemento quimico representado pelo simbolo O?",
+	            {"Ouro","Osmio","Oxigenio","Hidrogenio"},
+	            'C'
+	        },
+	
+	        {
+	            "Quem pintou a Mona Lisa?",
+	            {"Van Gogh","Picasso","Michelangelo","Leonardo da Vinci"},
+	            'D'
+	        },
+	
+	        {
+	            "Qual oceano banha a costa leste do Brasil?",
+	            {"Indico","Pacifico","Artico","Atlantico"},
+	            'D'
+	        },
+	
+	        {
+	            "Quantos lados possui um hexagono?",
+	            {"5","6","7","8"},
+	            'B'
+	        },
+	
+	        {
+	            "Qual destes animais eh um mamifero?",
+	            {"Tubarao","Pinguim","Golfinho","Tartaruga"},
+	            'C'
+	        },
+	
+	        {
+	            "Qual eh o menor numero primo?",
+	            {"0","1","2","3"},
+	            'C'
+	        }
+	
+	    };
+	
+	    char resposta;
+	    int acertos = 0;
+	    int i;
+	
+	    printf("\n==============================\n");
+	    printf("       NIVEIS DO QUIZ\n");
+	    printf("==============================\n");
+	
+	   for(i = 0; i < TOTAL; i++)
+{
+    if(i == 0)
+    {
+        printf("\n========== NIVEL 1 ==========\n");
+    }
+    else if(i == 3)
+    {
+        printf("\nParabens! Voce concluiu o nivel 1!\n");
+        printf("========== NIVEL 2 ==========\n");
+    }
+    else if(i == 6)
+    {
+        printf("\nParabens! Voce concluiu o nivel 2!\n");
+        printf("========== NIVEL 3 ==========\n");
+    }
 
-	printf("===== Quiz =====\n");
+    printf("\nPergunta %d\n", i + 1);
+    printf("%s\n", quiz[i].pergunta);
 
+    printf("A) %s\n", quiz[i].alternativas[0]);
+    printf("B) %s\n", quiz[i].alternativas[1]);
+    printf("C) %s\n", quiz[i].alternativas[2]);
+    printf("D) %s\n", quiz[i].alternativas[3]);
 
-	for (i = 0; i < TOTAL; i++) {
-		printf("\n%d - %s\n", i + 1, quiz[i].pergunta);
-		printf("a) %s\n", quiz[i].alternativas[0]);
-		printf("b) %s\n", quiz[i].alternativas[1]);
-		printf("c) %s\n", quiz[i].alternativas[2]);
-		printf("d) %s\n", quiz[i].alternativas[3]);
+    printf("\nResposta: ");
+    scanf(" %c", &resposta);
 
-		printf("\nInsira sua resposta: ");
-		scanf(" %c", &resposta);
-		
-		resposta = toupper(resposta);
+    resposta = toupper(resposta);
 
-		if (resposta == quiz[i].correta) {
-			printf("Resposta correta!\n");
-			acertos++;
-		} else {
-			printf("Resposta errada! A correta era %c.\n", quiz[i].correta);
-			erros++;
-		}
-	}
+    if(resposta == quiz[i].correta)
+    {
+        printf("Resposta correta!\n");
+        acertos++;
+    }
+    else
+    {
+        printf("\nResposta incorreta! Putz, que deslize.\n");
+        printf("A resposta correta era %c.\n", quiz[i].correta);
 
-	printf("\n===== RESULTADO FINAL =====\n");
-	printf("Voce acertou %d perguntas\n", acertos);
-	printf("Voce errou %d perguntas\n", erros);
-	printf("Sua porcentagem de acertos eh de %.0f%%\n", (acertos * 100.0) / TOTAL);
+        printf("\nVoce perdeu o desafio!\n");
+        printf("Acertos: %d de %d\n", acertos, TOTAL);
+        printf("Porcentagem: %.0f%%\n", (acertos * 100.0) / TOTAL);
+
+        repeticoesPergunta[i]++;
+
+        if(i <= 2)
+            repeticoesNivel[0]++;
+        else if(i <= 5)
+            repeticoesNivel[1]++;
+        else
+            repeticoesNivel[2]++;
+
+        return;
+    }
 }
-
-int main() {
-	
-	int opcao = 0;
-	
-	do {
-		system("cls");
 		
-		printf("==== MENU ====\n");
-		printf("\n1 - Instrucoes\n");
-		printf("2 - Jogar\n");
-		printf("3 - Sair\n");
-		printf("Escolha uma opcao: ");
-		scanf("%d", &opcao);
-		
-		switch(opcao){
-			case 1: 
-			printf("=== INSTRUCOES ===\n");
-			printf("\nResponda as perguntas com a resposta correta\n");
-			printf("Escolha a resposta correta nas alternativas\n");
-			break;
-			
-			case 2:
-			Quiz();
-			break;
-			
-			case 3: 
-			printf("Saindo...\n");
-		}
+	    printf("\n=================================\n");
+	    printf("MEUS PARABENS!\nVoce concluiu todos os niveis com sucesso!\n");
+	    printf("Acredito que ate Albert Einstein teria inveja da sua inteligencia haha!\n");
+	    printf("=================================\n");
+	
+	    printf("Acertos: %d de %d\n",acertos,TOTAL);
+	    printf("Porcentagem: %.0f%%\n",(acertos*100.0)/TOTAL);
+	    printf("Tentativas realizadas: %d\n", tentativas);
+	
 	}
-	while(opcao != 3);
 	
-	return 0;
-}
+	
+	int main(){
+	
+	    int opcao;
+	
+	    do{
+	
+	        system("cls");
+	
+	        printf("=========== MENU ===========\n");
+	        printf("1 - Instrucoes\n");
+	        printf("2 - Jogar\n");
+	        printf("3 - Sair\n");
+	        printf("============================\n");
+	
+	        printf("Escolha: ");
+	        scanf("%d",&opcao);
+	
+	        switch(opcao){
+	
+	            case 1:
+	                system("cls");
+	
+	                printf("=========== INSTRUCOES ===========\n");
+	                printf("Responda as perguntas corretamente.\n");
+	                printf("Cada acerto libera o proximo desafio.\n");
+	                printf("Se errar uma pergunta, o jogo termina.\n");
+	                printf("Lembrando que voce pode sempre tentar de novo.\n");
+	                printf("Boa sorte!\n");
+	
+	                system("pause");
+	                break;
+	
+	            case 2:
+	                system("cls");
+	
+	                Quiz();
+	
+	                system("pause");
+	                break;
+	
+	            case 3:
+	                printf("\nEncerrando o programa...\n");
+	                break;
+	
+	            default:
+	                printf("\nOpcao invalida!\n");
+	                system("pause");
+	        }
+	
+	    }while(opcao!=3);
+	
+	    return 0;
+	}
